b2d577ced16cfb8a00fe9cf7457cb8c6ca63d344
cexbrayat <cedric@ninja-squad.com>
2018-09-05 17:23:28 +0200

    a2418a9037 fix(ivy): use proper sanitizer names

---
+++ esm5/src/render3/view/util.js
@@ -0,0 +1,99 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import * as o from '../../output/output_ast';
+/** Name of the temporary to use during data binding */
+export var TEMPORARY_NAME = '_t';
+/** Name of the context parameter passed into a template function */
+export var CONTEXT_NAME = 'ctx';
+/** Name of the RenderFlag passed into a template function */
+export var RENDER_FLAGS = 'rf';
+/** The prefix reference variables */
+export var REFERENCE_PREFIX = '_r';
+/** The name of the implicit context reference */
+export var IMPLICIT_REFERENCE = '$implicit';
+/** Name of the i18n attributes **/
+export var I18N_ATTR = 'i18n';
+export var I18N_ATTR_PREFIX = 'i18n-';
+/** I18n separators for metadata **/
+export var MEANING_SEPARATOR = '|';
+export var ID_SEPARATOR = '@@';
+/**
+ * Creates an allocator for a temporary variable.
+ *
+ * A variable declaration is added to the statements the first time the allocator is invoked.
+ */
+export function temporaryAllocator(statements, name) {
+    var temp = null;
+    return function () {
+        if (!temp) {
+            statements.push(new o.DeclareVarStmt(TEMPORARY_NAME, undefined, o.DYNAMIC_TYPE));
+            temp = o.variable(name);
+        }
+        return temp;
+    };
+}
+export function unsupported(feature) {
+    if (this) {
+        throw new Error("Builder " + this.constructor.name + " doesn't support " + feature + " yet");
+    }
+    throw new Error("Feature " + feature + " is not supported yet");
+}
+export function invalid(arg) {
+    throw new Error("Invalid state: Visitor " + this.constructor.name + " doesn't handle " + o.constructor.name);
+}
+export function isI18NAttribute(name) {
+    return name === I18N_ATTR || name.startsWith(I18N_ATTR_PREFIX);
+}
+export function asLiteral(value) {
+    if (Array.isArray(value)) {
+        return o.literalArr(value.map(asLiteral));
+    }
+    return o.literal(value, o.INFERRED_TYPE);
+}
+export function conditionallyCreateMapObjectLiteral(keys) {
+    if (Object.getOwnPropertyNames(keys).length > 0) {
+        return mapToExpression(keys);
+    }
+    return null;
+}
+export function mapToExpression(map, quoted) {
+    if (quoted === void 0) { quoted = false; }
+    return o.literalMap(Object.getOwnPropertyNames(map).map(function (key) { return ({ key: key, quoted: quoted, value: asLiteral(map[key]) }); }));
+}
+/**
+ *  Remove trailing null nodes as they are implied.
+ */
+export function trimTrailingNulls(parameters) {
+    while (o.isNull(parameters[parameters.length - 1])) {
+        parameters.pop();
+    }
+    return parameters;
+}
+export function getQueryPredicate(query, constantPool) {
+    if (Array.isArray(query.predicate)) {
+        return constantPool.getConstLiteral(o.literalArr(query.predicate.map(function (selector) { return o.literal(selector); })));
+    }
+    else {
+        return query.predicate;
+    }
+}
+export function noop() { }
+var DefinitionMap = /** @class */ (function () {
+    function DefinitionMap() {
+        this.values = [];
+    }
+    DefinitionMap.prototype.set = function (key, value) {
+        if (value) {
+            this.values.push({ key: key, value: value, quoted: false });
+        }
+    };
+    DefinitionMap.prototype.toLiteralMap = function () { return o.literalMap(this.values); };
+    return DefinitionMap;
+}());
+export { DefinitionMap };
+//# sourceMappingURL=util.js.map
