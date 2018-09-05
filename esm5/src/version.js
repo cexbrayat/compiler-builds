b3659c8a53662bf215c3c502f2e1b623d85bb35a
Misko Hevery <misko@hevery.com>
2018-09-05 17:11:50 +0200

    51c0d9cae9 style(ivy): remove unused ivy code (#25780)

---
+++ esm5/src/version.js
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of the common package.
+ */
+import { Version } from './util';
+export var VERSION = new Version('7.0.0-beta.4-51c0d9cae9');
+//# sourceMappingURL=version.js.map
